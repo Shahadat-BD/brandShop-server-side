53dbac4bf3634d706b08c1c7da1beea16cabd7ea
Shahadat-BD <shahadatstudent34@gmail.com>
2023-10-20 17:05:11 +0000

    id edited of mycart

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ async function run() {
 
     app.get("/my-cart/:id",async(req,res)=>{
          const id = req.params.id
-         const query = {_id : id}
+         const query = {_id : new ObjectId(id)}
          const result = await myCartCollection.findOne(query)
          res.send(result)
     })
@@ -60,7 +60,7 @@ async function run() {
  
     app.delete('/my-cart/:id',async(req,res)=>{
       const id = req.params.id
-      const query = { _id : id };
+      const query = { _id : new ObjectId(id) };
       const result = await myCartCollection.deleteOne(query);
         res.send(result)
     })
